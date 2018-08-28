6ba137d330ea370daec09a384cb81e9a1d18a51f
Laurynas Butkus <laurynas@baltsoft.com>
2018-08-28 22:48:28 +0300

    Add PDF split and merge example

---
+++ examples/split_and_merge_pdf.rb
@@ -0,0 +1,20 @@
+require 'convert_api'
+require 'tmpdir'
+
+ConvertApi.configure do |config|
+  config.api_secret = ENV['CONVERT_API_SECRET'] # your api secret
+end
+
+# Example of extracting first and last pages from PDF and then merging them back to new PDF.
+# https://www.convertapi.com/pdf-to-split
+# https://www.convertapi.com/pdf-to-merge
+
+split_result = ConvertApi.convert('split', File: 'files/test.pdf')
+
+files = [split_result.files.first, split_result.files.last]
+
+merge_result = ConvertApi.convert('merge', Files: files)
+
+saved_files = merge_result.save_files(Dir.tmpdir)
+
+puts "The PDF saved to #{saved_files}"
