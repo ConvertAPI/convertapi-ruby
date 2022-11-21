ae07d8758cb6d846a9fb25efe633d501e2844ddb
Jonas Jasas <jonas@baltsoft.com>
2022-11-21 13:45:19 +0200

    Keyword argument fix

--- examples/create_pdf_thumbnail.rb
+++ examples/create_pdf_thumbnail.rb
@@ -11,17 +11,17 @@ end
 
 pdf_result = ConvertApi.convert(
   'extract',
-  File: 'files/test.pdf',
-  PageRange: 1,
+  {File: 'files/test.pdf',
+  PageRange: 1}
 )
 
 jpg_result = ConvertApi.convert(
   'jpg',
-  File: pdf_result,
+  {File: pdf_result,
   ScaleImage: true,
   ScaleProportions: true,
   ImageHeight: 300,
-  ImageWidth: 300,
+  ImageWidth: 300}
 )
 
 saved_files = jpg_result.save_files(Dir.tmpdir)
